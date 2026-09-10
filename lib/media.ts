b54b4a2b54d5659fcba2feaff6cf9777f646a474
Grosscan28 <m.falihsalomo28@gmail.com>
2026-09-10 15:38:03 +0700

    Fix filename case for Work-1 photo

--- lib/media.ts
+++ lib/media.ts
@@ -13,7 +13,7 @@ export const media = {
   aboutSecondary: mediaPath('media', 'TJ-About-Small.JPG'),
 
   workPhotos: [
-    mediaPath('Work-photos', 'Work-1.jpg'),
+    mediaPath('Work-photos', 'Work-1.JPG'),
     mediaPath('Work-photos', 'Work-2.jpg'),
     mediaPath('Work-photos', 'Work-3.jpg'),
     mediaPath('Work-photos', 'Work-4.jpg'),
