e1b428bfa88cef04a47a87bd1c1ee2ad5b21a152
Grosscan28 <m.falihsalomo28@gmail.com>
2026-09-10 15:06:00 +0700

    fix: load Photos assets from GitHub raw URLs

--- lib/media.ts
+++ lib/media.ts
@@ -1,39 +1,38 @@
 /** Centralized media configuration. */
 
+const GITHUB_MEDIA_BASE = 'https://raw.githubusercontent.com/Grosscan28/Landing-Page-TJ-SONS-SDN-BHD/redesign/hero-sabah-map/Photos'
+
+const mediaPath = (folder: string, file: string) => `${GITHUB_MEDIA_BASE}/${folder}/${file}`
+
 export const media = {
   heroVideo: '',
-  companyProfileVideo: '/media/TJ-company-video.mp4',
-  heroPoster: '/media/TJ-V-Group.JPG',
-  companyProfileThumb: '/media/TJ-Greater-Group.JPG',
-
-  aboutMain: '/media/TJ-About-Big.JPG',
-  aboutSecondary: '/media/TJ-About-Small.JPG',
-
+  companyProfileVideo: mediaPath('media', 'TJ-company-video.mp4'),
+  heroPoster: mediaPath('media', 'TJ-V-Group.JPG'),
+  companyProfileThumb: mediaPath('media', 'TJ-Greater-Group.JPG'),
+  aboutMain: mediaPath('media', 'TJ-About-Big.JPG'),
+  aboutSecondary: mediaPath('media', 'TJ-About-Small.JPG'),
   workPhotos: [
-    '/Work-photos/Work-2.JPG',
-    '/Work-photos/Work-3.JPG',
-    '/Work-photos/Work-1.JPG',
-    '/Work-photos/Work-4.JPG',
-    '/Work-photos/Work-5.JPG',
+    mediaPath('Work-photos', 'Work-2.JPG'),
+    mediaPath('Work-photos', 'Work-3.JPG'),
+    mediaPath('Work-photos', 'Work-1.JPG'),
+    mediaPath('Work-photos', 'Work-4.JPG'),
+    mediaPath('Work-photos', 'Work-5.JPG'),
   ],
-
   vendorLogos: [
-    '/vendor/CIDB.png',
-    '/vendor/Boustead.webp',
-    '/vendor/Sime-darby.jpg',
-    '/vendor/Felcra.png',
-    '/vendor/FGV.png',
-    '/vendor/IOI.jpg',
-    '/vendor/Felda.png',
-    '/vendor/Sawit.png',
-    '/vendor/Sedafiat.png',
-    '/vendor/Wilmar.png',
+    mediaPath('vendor', 'CIDB.png'),
+    mediaPath('vendor', 'Boustead.webp'),
+    mediaPath('vendor', 'Sime-darby.jpg'),
+    mediaPath('vendor', 'Felcra.png'),
+    mediaPath('vendor', 'FGV.png'),
+    mediaPath('vendor', 'IOI.jpg'),
+    mediaPath('vendor', 'Felda.png'),
+    mediaPath('vendor', 'Sawit.png'),
+    mediaPath('vendor', 'Sedafiat.png'),
+    mediaPath('vendor', 'Wilmar.png'),
   ],
 } as const
 
-export const companyProfile = {
-  videoDuration: '4:26',
-} as const
+export const companyProfile = { videoDuration: '4:26' } as const
 
 export const contact = {
   companyName: 'TJ & SONS HOLDINGS SDN BHD',
